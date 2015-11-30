aeebd4bf05282cb4b4f80483cfc9de43a736a943
Gabriela Medeiros Mendes Nandi <nandi.gabriela@gmail.com>
2015-11-30 02:18:28 -0200

    PRICES $$$

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,6 +8,7 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
+    puts "BATATA BATATA"
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name,
                                                             :email,
                                                             :password,
